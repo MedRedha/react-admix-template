0b676b1b98bb1ee7f186cff0dfb6a55adb4e25a9
MedRedha <redha.dirac@gmail.com>
2019-12-02 07:49:25 +0100

    Create DropDown.js
    creating a react-ui dropdown menu

---
+++ src/Components/DropDown.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Dropdown } from "semantic-ui-react";
+
+const options = [
+  { key: 1, text: "Choice 1", value: 1 },
+  { key: 2, text: "Choice 2", value: 2 },
+  { key: 3, text: "Choice 3", value: 3 }
+];
+
+const DropdownComponent = () => (
+  <Dropdown
+    clearable
+    placeholder="Select data"
+    search
+    options={options}
+    selection
+  />
+);
+
+export default DropdownComponent;
