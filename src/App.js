2bbc0a0c734f9a8a0f4998df9465b5dfb98baebb
Med Redha <redha.dirac@gmail.com>
2020-02-02 02:40:51 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -17,91 +17,129 @@ import battery from "./assets/battery.svg";
 import full from "./assets/full.svg";
 import profits from "./assets/profits.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="AppMenu">
-        <div className="SideMenuHeader">
-          <img className="Logo" src={Logo} alt="logo" />
+class App extends React.Component {
+  state = {
+    loading: true
+  };
+
+  componentDidMount() {
+    demoAsyncCall().then(() => this.setState({ loading: false }));
+  }
+
+  render() {
+    const { loading } = this.state;
+    if (loading) {
+      return (
+        <div className="Cube-container">
+          <div class="sk-cube-grid">
+            <div class="sk-cube sk-cube1"></div>
+            <div class="sk-cube sk-cube2"></div>
+            <div class="sk-cube sk-cube3"></div>
+            <div class="sk-cube sk-cube4"></div>
+            <div class="sk-cube sk-cube5"></div>
+            <div class="sk-cube sk-cube6"></div>
+            <div class="sk-cube sk-cube7"></div>
+            <div class="sk-cube sk-cube8"></div>
+            <div class="sk-cube sk-cube9"></div>
+            Scrumbling...
+          </div>
         </div>
-        <div>
-          <MenuButton icon={window} text={"my apps"} />
-          <MenuButton icon={graph} text={"reports"} />
-          <MenuButton icon={docs} text={"docs"} />
-          <MenuButton icon={download} text={"download"} />
-          <MenuButton icon={user} text={"my profile"} />
+      );
+    }
+    return (
+      <div className="App">
+        <div className="AppMenu">
+          <div className="SideMenuHeader">
+            <img className="Logo" src={Logo} alt="logo" />
+          </div>
+          <div>
+            <MenuButton icon={window} text={"my apps"} />
+            <MenuButton icon={graph} text={"reports"} />
+            <MenuButton icon={docs} text={"docs"} />
+            <MenuButton icon={download} text={"download"} />
+            <MenuButton icon={user} text={"my profile"} />
+          </div>
+        </div>
+        <div className="AppMain">
+          <div className="Top">
+            <h3 class="Title">Overview</h3>
+            <div className="DropDownContainer">
+              <DropdownComponent />
+            </div>
+          </div>
+          <div className="Tab">
+            <TabComponent
+              icon={profits}
+              text={"revenue"}
+              amount={"$510"}
+              rate={12}
+            />
+            <div class="VSeparator" styles="height: 45px;"></div>
+            <TabComponent
+              icon={battery}
+              text={"impressions"}
+              amount={"12.2m"}
+              rate={8}
+            />
+            <div class="VSeparator" styles="height: 45px;"></div>
+            <TabComponent
+              icon={full}
+              text={"fill rate"}
+              amount={"37.5%"}
+              rate={4}
+            />
+            <div class="VSeparator" styles="height: 45px;"></div>
+            <TabComponent
+              icon={award}
+              text={"ecpm"}
+              amount={"$2.7"}
+              rate={-5}
+            />
+          </div>
+          <div className="More">VIEW ALL DATA</div>
+          <div class="HSeparator" styles="width: 45px;"></div>
+          <Container>
+            <div className="MiddleContainer">
+              <div class="SecondTitle">MY APPS</div>
+              <img class="Sort" src={sort} alt="button" />
+            </div>
+            <div className="Center-Container">
+              <DetailsComponent
+                amount={`$224`}
+                AppName={"TrackDemoAWE"}
+                rate={16}
+              />
+              <DetailsComponent
+                amount={`$224`}
+                AppName={"RacingDemoAdmix-2"}
+                rate={-42}
+              />
+              <DetailsComponent
+                amount={`$224`}
+                AppName={"TrackDemoAWE"}
+                rate={-30}
+              />
+              <DetailsComponent
+                amount={`$224`}
+                AppName={"RacingDemoAdmix2"}
+                rate={30}
+              />
+              <DetailsComponent
+                amount={`$224`}
+                AppName={"RacingDemoAdmix"}
+                rate={0}
+                name={"Off"}
+              />
+            </div>
+          </Container>
         </div>
       </div>
-      <div className="AppMain">
-        <div className="Top">
-          <h3 class="Title">Overview</h3>
-          <div className="DropDownContainer">
-            <DropdownComponent />
-          </div>
-        </div>
-        <div className="Tab">
-          <TabComponent
-            icon={profits}
-            text={"revenue"}
-            amount={"$510"}
-            rate={12}
-          />
-          <div class="VSeparator" styles="height: 45px;"></div>
-          <TabComponent
-            icon={battery}
-            text={"impressions"}
-            amount={"12.2m"}
-            rate={8}
-          />
-          <div class="VSeparator" styles="height: 45px;"></div>
-          <TabComponent
-            icon={full}
-            text={"fill rate"}
-            amount={"37.5%"}
-            rate={4}
-          />
-          <div class="VSeparator" styles="height: 45px;"></div>
-          <TabComponent icon={award} text={"ecpm"} amount={"$2.7"} rate={-5} />
-        </div>
-        <div className="More">VIEW ALL DATA</div>
-        <div class="HSeparator" styles="width: 45px;"></div>
-        <Container>
-          <div className="MiddleContainer">
-            <div class="SecondTitle">MY APPS</div>
-            <img class="Sort" src={sort} alt="button" />
-          </div>
-          <div className="Center-Container">
-            <DetailsComponent
-              amount={`$224`}
-              AppName={"TrackDemoAWE"}
-              rate={16}
-            />
-            <DetailsComponent
-              amount={`$224`}
-              AppName={"RacingDemoAdmix-2"}
-              rate={-42}
-            />
-            <DetailsComponent
-              amount={`$224`}
-              AppName={"TrackDemoAWE"}
-              rate={-30}
-            />
-            <DetailsComponent
-              amount={`$224`}
-              AppName={"RacingDemoAdmix2"}
-              rate={30}
-            />
-            <DetailsComponent
-              amount={`$224`}
-              AppName={"RacingDemoAdmix"}
-              rate={0}
-              name={"Off"}
-            />
-          </div>
-        </Container>
-      </div>
-    </div>
-  );
+    );
+  }
+}
+
+function demoAsyncCall() {
+  return new Promise(resolve => setTimeout(() => resolve(), 2500));
 }
 
 export default App;
